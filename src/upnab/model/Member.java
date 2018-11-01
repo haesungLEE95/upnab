2180bc018ff0ac876add3170cff64cba21e365ed
Administrator <Administrator@WIN-6HA7QMCUJBF>
2018-11-01 15:40:35 +0900

    member 완성

--- src/upnab/model/Member.java
+++ src/upnab/model/Member.java
@@ -1,11 +1,56 @@
 package upnab.model;
 
 public class Member {
-	MEMBER_NUM
-	MEMBER_ID
-	MEMBER_PASSWORD
-	MEMBER_NAME
-	MEMBER_EMAIL
-	MEMBER_TEL
-	MEMBER_INOUT
+    private int member_num; 
+    private String member_id; 
+    private String member_password; 
+    private String member_name; 
+    private String member_email; 
+    private String member_tel; 
+    private String member_inout;
+    
+	public int getMember_num() {
+		return member_num;
+	}
+	public void setMember_num(int member_num) {
+		this.member_num = member_num;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getMember_password() {
+		return member_password;
+	}
+	public void setMember_password(String member_password) {
+		this.member_password = member_password;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public String getMember_email() {
+		return member_email;
+	}
+	public void setMember_email(String member_email) {
+		this.member_email = member_email;
+	}
+	public String getMember_tel() {
+		return member_tel;
+	}
+	public void setMember_tel(String member_tel) {
+		this.member_tel = member_tel;
+	}
+	public String getMember_inout() {
+		return member_inout;
+	}
+	public void setMember_inout(String member_inout) {
+		this.member_inout = member_inout;
+	}
+    
+    
 }
